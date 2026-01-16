7d75fde0d8cf4560c0e92c3d0798dfe3d2a3c8d9
Juan Lemes <juanlemes77@gmail.com>
2026-01-15 23:49:04 -0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
       className='bg-black flex flex-col justify-center items-center p-4 '
     >
       <p className='mb-2'>
-        Developed by{' '}
+        © {new Date().getFullYear()} Developed by{' '}
         <a
           href='https://github.com/jlemes1'
           target='_blank'
@@ -13,12 +13,8 @@ function Footer() {
           className='underline decoration-white'
         >
           Juan Lemes
-        </a>{' '}
-        - Project for portfolio.
-      </p>
-      <p>
-        This is a fan project and is not affiliated with or endorsed by Tame
-        Impala.
+        </a>
+        . Project for portfolio.
       </p>
     </footer>
   );
